2b48920188def7f0519b954e429f6884b42c3cb8
Doina Fitchevici <domna16@gmail.com>
2024-06-30 19:13:16 -0400

    Fixed Product model path

--- controllers/shoppingCartController.js
+++ controllers/shoppingCartController.js
@@ -1,4 +1,4 @@
-const Product = require("../models/product");
+const Product = require("../models/Product");
 const ShoppingCart = require("../models/ShoppingCart");
 const parseValidationErrors = require("../utils/parseValidationErrs");
 
--- controllers/shoppingListController.js
+++ controllers/shoppingListController.js
@@ -1,4 +1,4 @@
-const Product = require("../models/product");
+const Product = require("../models/Product");
 const ShoppingList = require("../models/ShoppingList");
 const parseValidationErrors = require("../utils/parseValidationErrs");
 
